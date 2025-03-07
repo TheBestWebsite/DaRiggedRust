3bcb388b1f06fd44ee16586919023265a3296d97
TheBestWebsite <164817940+TheBestWebsite@users.noreply.github.com>
2025-03-06 20:32:43 -0800

    Add checklist making logic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 fn main() {
-    println!("Welcome to my little game!\nYou shall not beat me.")
-    
+    println!("Welcome to my little game!\nYou shall not beat me.");
+    //let mut v: Vec<u8> = Vec::new()
+    let mut v = vec![false; 25];
+    for number in (1..25).rev() {
+        if number == 25 {
+            v[number - 1] = true
+        } else if v[number] == false && v[number + 2] == false && v[number + 3] == false {
+            v[number - 1] = true
+        } else {
+            v[number - 1] = false
+        }
+    }
 }
