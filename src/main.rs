7f3b09683d78db7af0be9e9b31fdaa542012814c
TheBestWebsite <164817940+TheBestWebsite@users.noreply.github.com>
2025-03-11 20:24:43 -0700

    Add number input functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 fn main() {
     println!("Welcome to my little game!\nYou shall not beat me.");
     //let mut v: Vec<u8> = Vec::new()
@@ -13,5 +14,29 @@ fn main() {
         } else {
             v[number] = false;
         }
+    }
+    let mut total: u32 = 4;
+    println!(
+        "I call first! I start with {}. On your turn, type 1, 3, or 4. I will add it for you. If you try any funny business, I will panic.",
+        total
+    );
+    total = get_number(total);
+}
+
+fn get_number(total: u32) -> u32 {
+    let mut input = String::with_capacity(1);
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line - I blame you.");
+    let number: i32 = input
+        .trim()
+        .parse()
+        .expect("That isn't a number - I hate you!");
+    if number == 1 || number == 3 || number == 4 {
+        let number: u32 = number as u32;
+        total + number
+    } else {
+        println!("Bad number");
+        get_number(total)
     }
 }
