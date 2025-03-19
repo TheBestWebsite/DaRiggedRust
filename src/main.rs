84afde962d8e27c1944779a0d788d7593268d4d8
TheBestWebsite <164817940+TheBestWebsite@users.noreply.github.com>
2025-03-18 19:06:23 -0700

    Change "v" to "checklist"

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::io;
 fn main() {
     println!("Welcome to my little game!\nYou shall not beat me.");
-    let v = make_checklist();
-    let mut total: u32 = calc_optimal_move(0, &v);
+    let checklist = make_checklist();
+    let mut total: u32 = calc_optimal_move(0, &checklist);
     println!(
         "I call first! I start with {}. On your turn, type 1, 3, or 4. I will add it for you.",
         total
@@ -11,10 +11,10 @@ fn main() {
         total = get_number(total);
         println!(
             "I play {}, bringing the total to {}.",
-            calc_optimal_move(total, &v),
-            calc_optimal_move(total, &v) + total
+            calc_optimal_move(total, &checklist),
+            calc_optimal_move(total, &checklist) + total
         );
-        total = calc_optimal_move(total, &v) + total;
+        total = calc_optimal_move(total, &checklist) + total;
         if total >= 25 {
             break;
         }
@@ -27,10 +27,7 @@ fn get_number(total: u32) -> u32 {
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line - I blame you.");
-    let number: i32 = input
-        .trim()
-        .parse()
-        .unwrap_or(0);
+    let number: i32 = input.trim().parse().unwrap_or(0);
     if (number == 1 || number == 3 || number == 4) && number as u32 + total <= 25 {
         let number: u32 = number as u32;
         total + number
@@ -40,12 +37,12 @@ fn get_number(total: u32) -> u32 {
     }
 }
 
-fn calc_optimal_move(total: u32, v: &Vec<bool>) -> u32 {
-    if total + 4 <= v.len() as u32 - 1 && v[(total + 4) as usize] {
+fn calc_optimal_move(total: u32, checklist: &Vec<bool>) -> u32 {
+    if total + 4 <= checklist.len() as u32 - 1 && checklist[(total + 4) as usize] {
         4
-    } else if total + 3 <= v.len() as u32 - 1 && v[(total + 3) as usize] {
+    } else if total + 3 <= checklist.len() as u32 - 1 && checklist[(total + 3) as usize] {
         3
-    } else if v[(total + 1) as usize] {
+    } else if checklist[(total + 1) as usize] {
         1
     } else {
         0
@@ -53,20 +50,20 @@ fn calc_optimal_move(total: u32, v: &Vec<bool>) -> u32 {
 }
 
 fn make_checklist() -> Vec<bool> {
-    let mut v: Vec<bool> = vec![false; 26];
+    let mut checklist: Vec<bool> = vec![false; 26];
     for number in (0..26).rev() {
         if number == 25 {
-            v[number] = true;
-        } else if v[number + 1]
-            || (number + 3 <= v.len() - 1 && v[number + 3])
-            || (number + 4 <= v.len() - 1 && v[number + 4])
+            checklist[number] = true;
+        } else if checklist[number + 1]
+            || (number + 3 <= checklist.len() - 1 && checklist[number + 3])
+            || (number + 4 <= checklist.len() - 1 && checklist[number + 4])
         {
-            v[number] = false;
+            checklist[number] = false;
         } else {
-            v[number] = true;
+            checklist[number] = true;
         }
         // Debug code
-        // println!("{}, {}", number, v[number]);
+        // println!("{}, {}", number, checklist[number]);
     }
-    v
+    checklist
 }
